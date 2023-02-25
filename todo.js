3f7b5b03f7bf44b63b7cd49539810bcd2fe05783
Marvin Hackfort <marvin.hackfort@gmail.com>
2023-02-25 15:48:05 +0100

    new functions

---
+++ todo.js
@@ -0,0 +1,25 @@
+
+function addItemFunction(){ 
+    const newItem = document.getElementById("newItem");
+    console.log(newItem.value);
+    const ul = document.getElementById("myToDo");
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(newItem.value));
+    ul.appendChild(li);
+    //this adds multiple classes from bootstrap
+    li.classList.add("list-group-item", "list-group-item-dark", "list-group-item-action"); 
+
+    //clear value of input field newItem
+    newItem.value ="";
+
+    // add event listener to the li element to listen for a click event and remove the li element when clicked
+        li.addEventListener('click', function() {
+            ul.removeChild(li);
+        });
+}
+const newItem = document.getElementById("newItem");
+newItem.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        addItemFunction();
+    }
+});
